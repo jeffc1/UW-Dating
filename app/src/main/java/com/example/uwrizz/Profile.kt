1ab48c78ffea8776635ab9a3a82fb8b4fb7e4425
Alvin Hu <y472hu@uwaterloo.ca>
2024-03-18 14:07:06 -0400

    updated Profile.kt

--- app/src/main/java/com/example/uwrizz/Profile.kt
+++ app/src/main/java/com/example/uwrizz/Profile.kt
@@ -4,10 +4,6 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.Button
-import androidx.compose.material.Icon
-import androidx.compose.material.OutlinedTextField
-import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -15,11 +11,9 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.*
 import androidx.compose.foundation.clickable
-import androidx.compose.material.MaterialTheme
 import androidx.compose.ui.res.vectorResource
-import androidx.compose.material.ButtonDefaults
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
@@ -28,19 +22,63 @@ import androidx.activity.result.contract.ActivityResultContracts
 import coil.compose.rememberImagePainter
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
-import androidx.compose.material.DropdownMenu
-import androidx.compose.material.DropdownMenuItem
+
+
 import com.example.uwrizz.R
 
+
+@Composable
+fun AgeSelector(
+    modifier: Modifier = Modifier,
+    ageRange: ClosedFloatingPointRange<Float> = 18f..30f,
+    initialAge: Float,
+    onAgeSelected: (Float) -> Unit,
+    label: String
+) {
+    var age by remember { mutableStateOf(initialAge) }
+    var showSlider by remember { mutableStateOf(false) } // State to control the visibility of the slider
+
+    Column(modifier = modifier) {
+        OutlinedTextField(
+            value = "Age: ${age.toInt()}",
+            onValueChange = {},
+            label = { Text(label) },
+            readOnly = true, // Makes it non-editable
+            trailingIcon = {
+                Icon(Icons.Default.ArrowDropDown, "Select Age", Modifier.clickable { showSlider = !showSlider })
+            },
+            modifier = Modifier
+                .clickable { showSlider = !showSlider }
+                .fillMaxWidth().padding(top = 16.dp)
+        )
+
+        // Show the Slider when the OutlinedTextField is clicked
+        if (showSlider) {
+            Slider(
+                value = age,
+                onValueChange = { age = it },
+                valueRange = ageRange,
+                steps = (ageRange.endInclusive.toInt() - ageRange.start.toInt()) - 1,
+                onValueChangeFinished = {
+                    onAgeSelected(age)
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp)
+            )
+        }
+    }
+}
 
 @Composable
 fun ProfileSettingsScreen(
     profileImage: ImageVector, // Placeholder for profile image
     onImageClick: () -> Unit, // Placeholder click action for adding an image
     onImageSelected: (Uri) -> Unit,
-    onNavigateToPreferences: () -> Unit
+    onNavigateToPreferences: () -> Unit,
 ) {
     var imageUri by remember { mutableStateOf<Uri?>(null) }
     var imageUri1 by remember { mutableStateOf<Uri?>(null) }
@@ -52,10 +90,10 @@ fun ProfileSettingsScreen(
     var firstname by remember { mutableStateOf("") }
     var lastname by remember { mutableStateOf("") }
     var bio by remember { mutableStateOf("") }
-    var gender by remember { mutableStateOf("") }
-    var program by remember { mutableStateOf("") }
     var hobby by remember { mutableStateOf("") }
     var work by remember { mutableStateOf("") }
+    var age by remember { mutableStateOf(18f) } // Default initial age
+    var showAgeSlider by remember { mutableStateOf(false) }
 
     // Gender dropdown states
     var expandedGender by remember { mutableStateOf(false) }
@@ -144,6 +182,7 @@ fun ProfileSettingsScreen(
 
         // Existing fields
         Text("Profile Settings", style = MaterialTheme.typography.h5)
+
         OutlinedTextField(
             value = firstname,
             onValueChange = { firstname = it },
@@ -156,6 +195,18 @@ fun ProfileSettingsScreen(
             label = { Text("Last Name") },
             modifier = Modifier.fillMaxWidth().padding(top = 16.dp)
         )
+
+        //age
+        AgeSelector(
+            ageRange = 18f..30f, // This is the range of the slider
+            initialAge = age, // Pass the initial age, it could be a state if you need to remember it
+            onAgeSelected = {
+                age = it // Update the age when the user has finished selecting a new age
+                showAgeSlider = false // Hide the slider
+            },
+            label = "Select Age"
+        )
+
         OutlinedTextField(
             value = bio,
             onValueChange = { bio = it },
@@ -168,7 +219,7 @@ fun ProfileSettingsScreen(
             label = { Text("Gender") },
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(top = 8.dp)
+                .padding(top = 16.dp)
                 .clickable { expandedGender = true },
             trailingIcon = {
                 Icon(
@@ -198,7 +249,7 @@ fun ProfileSettingsScreen(
             label = { Text("Program") },
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(top = 8.dp)
+                .padding(top = 16.dp)
                 .clickable { expandedProgram = true },
             trailingIcon = {
                 Icon(
@@ -234,11 +285,36 @@ fun ProfileSettingsScreen(
             label = { Text("Work") },
             modifier = Modifier.fillMaxWidth().padding(top = 16.dp)
         )
+        // Save button
+        Button(
+            onClick = {
+                // Add logic here to save the profile settings
+                // You can use the values of the mutable state variables like firstname, lastname, etc. to update the user's profile in the database
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 24.dp)
+        ) {
+            Text("Save")
+        }
     }
 }
+//Database interaction
+
+data class UserProfile(
+    val id: Long = 0,
+    val firstName: String,
+    val lastName: String,
+    val bio: String,
+    val gender: String,
+    val program: String,
+    val hobby: String,
+    val work: String,
+    val profilePictureUri: String? // Store the URI of the profile picture
+)
 
 
-
+//Image code
 enum class ImageSource {
     Gallery,
     Camera
@@ -284,12 +360,25 @@ fun ImageUploadButton(
     }
 }
 
+
 //Preference page
 @Composable
 fun PreferencesScreen(
     onNavigateToProfile: () -> Unit
 ) {
     val scrollState = rememberScrollState()
+
+    var age by remember { mutableStateOf(18f) } // Default initial age
+    var showAgeSlider by remember { mutableStateOf(false) }
+
+    var expandedGender2 by remember { mutableStateOf(false) }
+    val genderoptions2 = listOf("Male", "Female", "Other", "Prefer not to say") // Define your options here
+    var selectGender2 by remember {mutableStateOf("Gender") }
+
+    var expandedProgram2 by remember { mutableStateOf(false) }
+    val programOptions2 = listOf("Arts", "Engineering", "Environment", "Health", "Mathematics", "Science") // Define your options here
+    var selectedProgram2 by remember {mutableStateOf("Program") }
+
     Column(
         modifier = Modifier
             .verticalScroll(scrollState) // This adds the scrolling behavior
@@ -309,6 +398,101 @@ fun PreferencesScreen(
                 Spacer(Modifier.width(4.dp)) // Add some spacing between the icon and the text
                 Text("Profile") // Text following the icon
             }
+        }
+
+        Spacer(modifier = Modifier.height(50.dp))
+        Text("Preference Settings", style = MaterialTheme.typography.h5)
+        OutlinedTextField(
+            value = selectGender2,
+            onValueChange = { /* ReadOnly TextField */ },
+            label = { Text("I'm Interested In: ") },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 8.dp)
+                .clickable { expandedGender2 = true },
+            trailingIcon = {
+                Icon(
+                    imageVector = Icons.Filled.ArrowDropDown,
+                    contentDescription = "Dropdown",
+                    Modifier.clickable { expandedGender2 = true }
+                )
+            },
+            readOnly = true // Make TextField readonly
+        )
+        DropdownMenu(
+            expanded = expandedGender2,
+            onDismissRequest = { expandedGender2 = false }
+        ) {
+            genderoptions2.forEach { gender ->
+                DropdownMenuItem(onClick = {
+                    selectGender2 = gender
+                    expandedGender2 = false
+                }) {
+                    Text(text = gender)
+                }
+            }
+        }
+
+        OutlinedTextField(
+            value = selectedProgram2,
+            onValueChange = { /* ReadOnly TextField */ },
+            label = { Text("I'm Interested In: ") },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 8.dp)
+                .clickable { expandedProgram2 = true },
+            trailingIcon = {
+                Icon(
+                    imageVector = Icons.Filled.ArrowDropDown,
+                    contentDescription = "Dropdown",
+                    Modifier.clickable { expandedProgram2 = true }
+                )
+            },
+            readOnly = true // Make TextField readonly
+        )
+        DropdownMenu(
+            expanded = expandedProgram2,
+            onDismissRequest = { expandedProgram2 = false }
+        ) {
+            programOptions2.forEach { program ->
+                DropdownMenuItem(onClick = {
+                    selectedProgram2 = program
+                    expandedProgram2 = false
+                }) {
+                    Text(text = program)
+                }
+            }
+        }
+        AgeSelector(
+            ageRange = 18f..30f, // This is the range of the slider
+            initialAge = age, // Pass the initial age, it could be a state if you need to remember it
+            onAgeSelected = {
+                age = it // Update the age when the user has finished selecting a new age
+                showAgeSlider = false // Hide the slider
+            },
+            label = "Select Preferred Minimum Age"
+        )
+        AgeSelector(
+            ageRange = 18f..30f, // This is the range of the slider
+            initialAge = age, // Pass the initial age, it could be a state if you need to remember it
+            onAgeSelected = {
+                age = it // Update the age when the user has finished selecting a new age
+                showAgeSlider = false // Hide the slider
+            },
+            label = "Select Preferred Maximum Age"
+        )
+
+        // Save button
+        Button(
+            onClick = {
+                // Add logic here to save the profile settings
+                // You can use the values of the mutable state variables like firstname, lastname, etc. to update the user's profile in the database
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 24.dp)
+        ) {
+            Text("Save")
         }
     }
 }
